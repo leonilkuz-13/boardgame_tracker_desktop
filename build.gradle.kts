4cd9146f191406aa8b0124925bd16c82ac02544b
Leonid <leonilfilippenkov@gmail.com>
2026-04-08 03:02:51 +0300

    update block dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
